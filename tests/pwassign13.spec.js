26a1c61dc12ef9759730046b7313bc16b89249b8
salmanfaiz786 <salman.faiz85@gmail.com>
2024-05-29 16:32:16 +0500

    Playwright assignment 13

---
+++ tests/pwassign13.spec.js
@@ -0,0 +1,42 @@
+const{test, expect} = require('@playwright/test')
+
+test('My First test', async({page}) => {
+    await page.goto('https://ecommerce-playground.lambdatest.io/')
+    await page.getByRole('button', { name: 'My account' }).hover();
+    await page.getByRole('link', { name: 'Register', exact: true }).click();
+    await page.getByPlaceholder('First Name').fill('Salman');
+    await page.getByPlaceholder('Last Name').fill('Faiz');
+    await page.getByPlaceholder('E-Mail').fill('salman.faiz70@gmail.com');
+    await page.getByPlaceholder('Telephone').fill('923454088840');
+    await page.getByPlaceholder('Password', { exact: true}).fill('P@ss1234');
+    await page.getByPlaceholder('Password Confirm' , {exact: true}).fill('P@ss1234');
+    await page.getByText('I have read and agree to the').click();
+    await expect( page.getByRole('button', { name: 'Continue' })).toBeVisible();
+    await page.getByRole('button', { name: 'Continue' }).click();
+    await expect (page.getByRole('heading', {name: 'Your Account Has Been Created!'})).toHaveText('Your Account Has Been Created!');
+    await page.waitForTimeout(5000);
+})
+
+
+
+/*const { chromium } = require('playwright');
+
+(async () => {
+  const browser = await chromium.launch({
+    headless: false
+  });
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto('https://ecommerce-playground.lambdatest.io/');
+  await page.getByRole('textbox', { name: 'Search For Products' }).click();
+  await page.getByRole('textbox', { name: 'Search For Products' }).fill('laptops');
+  await page.getByRole('textbox', { name: 'Search For Products' }).press('Enter');
+  await page.getByPlaceholder('Keywords').click();
+  await page.getByPlaceholder('Keywords').click({
+    button: 'right'
+  });
+
+  // ---------------------
+  await context.close();
+  await browser.close();
+})();*/
